3bfd4fd178c865894e52b0b82c838ee3077de2f9
Administrator <Administrator@DESKTOP-2JEDDB9>
2017-10-21 13:53:34 +0800

    新增备注

--- DbContextBulkExtensions.cs
+++ DbContextBulkExtensions.cs
@@ -7,6 +7,14 @@ namespace Des.EntityFrameworkCore.Extensions
 {
     public static class DbContextBulkExtensions
     {
+        /// <summary>
+        /// 批量新增
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="context"></param>
+        /// <param name="entities"></param>
+        /// <param name="bulkConfig"></param>
+        /// <param name="progress"></param>
         public static void BulkInsert<T>(this DbContext context, IList<T> entities, BulkConfig bulkConfig = null, Action<decimal> progress = null) where T : class
         {
             DbContextBulkTransaction.Execute(context, entities, OperationType.Insert,  bulkConfig, progress);
